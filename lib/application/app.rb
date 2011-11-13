32d9227c10fa50cefcaa9965b68e75df66b380f2
Erick Schmitt <ejschmitt@gmail.com>
2011-11-13 13:44:09 -0500

    adding executable

---
+++ lib/application/app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+class DelayedJobWeb < Sinatra::Base
+
+  set :static, true                             # set up static file routing
+  set :public, File.expand_path('..', __FILE__) # set up the static dir (with images/js/css inside)
+  set :views,  File.expand_path('../views', __FILE__) # set up the views dir
+
+  # Your "actions" go here…
+  #
+  get '/' do
+    haml :'/index'
+  end
+
+end
+
+# Run the app!
+#
+puts "Hello, you're running your web app from a gem!"
+DelayedJobWeb.run!
