00986c3efcd0e2cf870fc15cfbb6525ae551985f
Erick Schmitt <ejschmitt@gmail.com>
2011-11-16 20:30:05 -0500

    Version bump to 0.1.3

--- lib/delayed_job_web/application/app.rb
+++ lib/delayed_job_web/application/app.rb
@@ -4,15 +4,34 @@ require 'active_record'
 require 'delayed_job'
 require 'haml'
 
-configure do
+configure :development do
   puts "Configuring delayed_job_web"
   Delayed::Worker.backend = :active_record
   config = YAML::load(File.open('config/database.yml'))
   environment = Sinatra::Application.environment.to_s
   ActiveRecord::Base.logger = Logger.new($stdout)
   ActiveRecord::Base.establish_connection(
-    SHARED_DATABASE_URL || config[environment]
+    config[environment]
   )
+end
+
+configure :production do
+  puts "Configuring delayed_job_web"
+  db = ENV["DATABASE_URL"]
+  if db.match(/postgres:\/\/(.*):(.*)@(.*)\/(.*)/) 
+    username = $1
+    password = $2
+    hostname = $3
+    database = $4
+
+    ActiveRecord::Base.establish_connection(
+      :adapter  => 'postgresql',
+      :host     => hostname,
+      :username => username,
+      :password => password,
+      :database => database
+    )
+  end
 end
 
 class DelayedJobWeb < Sinatra::Base
