7407cafa7710ed87ff4096ec3a2a3361c414f329
Erick Schmitt <ejschmitt@gmail.com>
2011-11-16 20:59:25 -0500

    Version bump to 0.1.5

--- lib/delayed_job_web/application/app.rb
+++ lib/delayed_job_web/application/app.rb
@@ -37,7 +37,7 @@ end
 class DelayedJobWeb < Sinatra::Base
 
   set :static, true                             # set up static file routing
-  set :public_folder, File.expand_path('..', __FILE__) # set up the static dir (with images/js/css inside)
+  set :public, File.expand_path('..', __FILE__) # set up the static dir (with images/js/css inside)
   set :views,  File.expand_path('../views', __FILE__) # set up the views dir
   set :haml, { :format => :html5 }
 
