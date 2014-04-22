61fcb0dc75b6fc67e5be8b1e73519169126ffc26
Beth <beth@bethesque.com>
2014-04-23 07:18:12 +1000

    Converted BDD style test to TestCase style

--- test/lib/delayed_job_web/application/test_app.rb
+++ test/lib/delayed_job_web/application/test_app.rb
@@ -2,69 +2,66 @@ require 'test_helper'
 require 'support/delayed_job_fake'
 require 'delayed_job_web/application/app'
 
-describe DelayedJobWeb do
+class TestDelayedJobWeb < MiniTest::Unit::TestCase
 
   include Rack::Test::Methods
 
-  let(:app) { DelayedJobWeb }
-  let(:rack_env) { {'rack.session' => {:csrf => "123"}} }
-  let(:request_data) { {"authenticity_token" => "123"} }
-
-  describe '/requeue/all' do
-
-    let(:time) { Time.now }
-    let(:dataset) { Minitest::Mock.new }
-    let(:where) do
-      lambda { | criteria |
-        criteria.must_equal 'last_error IS NOT NULL'
-        dataset
-      }
-    end
-
-    before do
-      dataset.expect(:update_all, nil, [:run_at => time, :failed_at => nil])
-    end
-
-    it 'requeues all jobs that have errors' do
-
-      Time.stub(:now, time) do
-        Delayed::Job.stub(:where, where) do
-          post "/requeue/all", request_data, rack_env
-          last_response.status.must_equal 302
-        end
-      end
-
-      dataset.verify
-    end
+  def app
+    DelayedJobWeb
   end
 
-  describe '/requeue/:id' do
+  def test_requeue_all
 
-    let(:time) { Time.now }
-    let(:job) { Minitest::Mock.new }
-    let(:find) do
-      lambda { | id |
-        id.must_equal "1"
-        job
-      }
-    end
+    dataset = Minitest::Mock.new
+    where = lambda { | criteria |
+      criteria.must_equal 'last_error IS NOT NULL'
+      dataset
+    }
 
-    before do
-      job.expect(:update_attributes, nil, [:run_at => time, :failed_at => nil])
-    end
+    dataset.expect(:update_all, nil, [:run_at => time, :failed_at => nil])
 
-    it 'requeues the specified job' do
-
-      Time.stub(:now, time) do
-        Delayed::Job.stub(:find, find) do
-          post "/requeue/1", request_data, rack_env
-          last_response.status.must_equal 302
-        end
+    Time.stub(:now, time) do
+      Delayed::Job.stub(:where, where) do
+        post "/requeue/all", request_data, rack_env
+        last_response.status.must_equal 302
       end
-
-      job.verify
-
     end
+
+    dataset.verify
 
   end
+
+  def test_requeue_id
+    job = Minitest::Mock.new
+    job.expect(:update_attributes, nil, [:run_at => time, :failed_at => nil])
+
+    find = lambda { | id |
+      id.must_equal "1"
+      job
+    }
+
+    Time.stub(:now, time) do
+      Delayed::Job.stub(:find, find) do
+        post "/requeue/1", request_data, rack_env
+        last_response.status.must_equal 302
+      end
+    end
+
+    job.verify
+  end
+
+  private
+
+  def time
+    @time ||= Time.now
+  end
+
+  def rack_env
+    {'rack.session' => {:csrf => "123"}}
+  end
+
+  def request_data
+    {"authenticity_token" => "123"}
+  end
+
 end
